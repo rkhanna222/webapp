f9ae98b6ddef4dbdc04e532e171be604f7f2a55f
Raghav Khanna <khanna.ra@northeastern.edu>
2022-11-02 22:30:46 -0400

    Reboot script

--- ami/reboot.sh
+++ ami/reboot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # echo "Running Application"
-
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
 java -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar --spring.config.location=file:////home/ubuntu/application.properties
